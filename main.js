5680630de890f9ba7e367b6ce9e3f2118c2d40a2
arreba-star <75185381+arreba-star@users.noreply.github.com>
2021-01-18 10:36:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,255 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(1500 ,750);
+    canvas.position(110 , 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw(){
+     
+    fill(120 , 23 , 120)
+    rect(100 , 100 , 1300 , 580 , 30)
+
+
+    fill(0 , 50 , 128);
+    ellipse(200, 220, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(200, 320, 55, 55);
+
+    fill(0 , 50 , 128);
+    ellipse(200, 420, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(200, 520, 55, 55);
+
+    fill(0 , 50 , 128);
+    ellipse(200, 620, 55, 55);
+
+    // dsfdf
+
+    fill(0 , 50 , 128);
+    ellipse(1300, 220, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(1300, 320, 55, 55);
+
+    fill(0 , 50 , 128);
+    ellipse(1300, 420, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(1300, 520, 55, 55);
+    
+    fill(0 , 50 , 128);
+    ellipse(1300, 620, 55, 55);
+
+
+    fill(128 , 0 ,0)
+    ellipse(1200, 620, 55, 55);
+
+    fill(0 , 56 ,124)
+    ellipse(1200, 520, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(1200, 420, 55, 55);
+       
+    fill(0 , 56 ,124)
+    ellipse(1200, 320, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(1200, 220, 55, 55);
+
+    
+
+
+    fill(0 , 0 ,128)
+    ellipse(1100, 620, 55, 55);
+
+    fill(128 , 56 , 0)
+    ellipse(1100, 520, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(1100, 420, 55, 55);
+       
+    fill(128 , 56 ,0)
+    ellipse(1100, 320, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(1100, 220, 55, 55);
+
+    
+
+
+    fill(128 , 0 ,0)
+    ellipse(1000, 620, 55, 55);
+
+    fill(0 , 56 ,124)
+    ellipse(1000, 520, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(1000, 420, 55, 55);
+       
+    fill(0 , 56 ,124)
+    ellipse(1000, 320, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(1000, 220, 55, 55);
+
+
+
+
+
+    fill(0 , 50 , 128);
+    ellipse(900, 220, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(900, 320, 55, 55);
+
+    fill(0 , 50 , 128);
+    ellipse(900, 420, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(900, 520, 55, 55);
+    
+    fill(0 , 50 , 128);
+    ellipse(900, 620, 55, 55);
+
+
+    fill(128 , 0 ,0)
+    ellipse(800, 620, 55, 55);
+
+    fill(0 , 56 ,124)
+    ellipse(800, 520, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(800, 420, 55, 55);
+       
+    fill(0 , 56 ,124)
+    ellipse(800, 320, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(800, 220, 55, 55);
+
+    
+
+
+    fill(0 , 0 ,128)
+    ellipse(700, 620, 55, 55);
+
+    fill(128 , 56 , 0)
+    ellipse(700, 520, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(700, 420, 55, 55);
+       
+    fill(128 , 56 ,0)
+    ellipse(700, 320, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(700, 220, 55, 55);
+
+
+
+    fill(0 , 50 , 128);
+    ellipse(600, 220, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(600, 320, 55, 55);
+
+    fill(0 , 50 , 128);
+    ellipse(600, 420, 55, 55);
+
+    fill(120 , 50 , 0);
+    ellipse(600, 520, 55, 55);
+    
+    fill(0 , 50 , 128);
+    ellipse(600, 620, 55, 55);
+
+
+    fill(128 , 0 ,0)
+    ellipse(500, 620, 55, 55);
+
+    fill(0 , 56 ,124)
+    ellipse(500, 520, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(500, 420, 55, 55);
+       
+    fill(0 , 56 ,124)
+    ellipse(500, 320, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(500, 220, 55, 55);
+
+    
+
+
+    fill(0 , 0 ,128)
+    ellipse(400, 620, 55, 55);
+
+    fill(128 , 56 , 0)
+    ellipse(400, 520, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(400, 420, 55, 55);
+       
+    fill(128 , 56 ,0)
+    ellipse(400, 320, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(400, 220, 55, 55);
+
+
+    
+    fill(128 , 0 ,0)
+    ellipse(300, 620, 55, 55);
+
+    fill(0 , 56 ,124)
+    ellipse(300, 520, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(300, 420, 55, 55);
+       
+    fill(0 , 56 ,124)
+    ellipse(300, 320, 55, 55);
+
+    fill(128 , 0 ,0)
+    ellipse(300, 220, 55, 55);
+
+    
+
+
+    fill(0 , 0 ,128)
+    ellipse(200, 620, 55, 55);
+
+    fill(128 , 56 , 0)
+    ellipse(200, 520, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(200, 420, 55, 55);
+       
+    fill(128 , 56 ,0)
+    ellipse(200, 320, 55, 55);
+
+    fill(0 , 0 ,128)
+    ellipse(200, 220, 55, 55);
+
+    // Draw a rectangle at location (30, 20) with a width and height of 55.
+    fill(40 , 90 , 40)
+rect(0, 0, 1450, 55);
+rect(0, 700, 1450, 55);
+rect(0 ,20 , 55 ,800 );
+rect(1450 ,0 , 54634657 ,800 );
+
+
+    image(video, 600, 280, 300 , 300);
+
+}
+
+function take_snapshot(){
+    save('student_name.png');
+}
